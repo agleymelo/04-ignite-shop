bb7aca0a6935c09b1c39652fd0084e3dcaab3ce5
Agleylson Melo <agleylsonmelo@outlook.com>
2022-10-11 18:56:24 -0300

    Estrutura visual - Configurando Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  backgroundColor: "Purple",
+  borderRadius: 4,
+});
+
 export default function Home() {
   return (
     <div>
       <h1>hello world</h1>
+
+      <Button>Enviar</Button>
     </div>
-  )
+  );
 }
