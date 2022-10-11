5c2fca68823c122df158af1eb90fd9bcdba5b11f
Agleylson Melo <agleylsonmelo@outlook.com>
2022-10-11 20:26:17 -0300

    Estrutura visual - Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,29 @@
-import { styled } from "../styles";
+import Image from "next/future/image";
+import { HomeContainer, Product } from "../styles/pages/home";
 
-const Button = styled("button", {
-  backgroundColor: "Purple",
-  borderRadius: 4,
-});
+import Camisa1 from "../assets/1.png";
+import Camisa2 from "../assets/2.png";
+import Camisa3 from "../assets/3.png";
 
 export default function Home() {
   return (
-    <div>
-      <h1>hello world</h1>
+    <HomeContainer>
+      <Product>
+        <Image src={Camisa1} alt="" width={520} height={480} />
 
-      <Button>Enviar</Button>
-    </div>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={Camisa2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
