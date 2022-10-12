b5903ad474adf6d07f46403752c3f199ef18791e
Agleylson Melo <agleylsonmelo@outlook.com>
2022-10-12 18:49:01 -0300

    Produto & Checkout - SSG com parametro dinamico

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/future/image";
 import Stripe from "stripe";
 import { stripe } from "../../lib/stripe";
@@ -36,6 +36,19 @@ export default function Product({ product }: ProductProps) {
     </ProductContainer>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: {
+          id: "prod_Mb4LFrqRY1iDd5",
+        },
+      },
+    ],
+    fallback: false,
+  };
+};
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
   params,
