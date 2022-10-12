081aead4004dbb34dd445b1a2cd7ed531da29c08
Agleylson Melo <agleylsonmelo@outlook.com>
2022-10-12 17:52:01 -0300

    Produto & Checkout - Estrutura da pagina de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,31 @@
-import { useRouter } from 'next/router'
+import Image from "next/future/image";
+import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
 export default function Product() {
-  const { query } = useRouter()
+  const { query } = useRouter();
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer>{/* <Image /> */}</ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>USD 20.00</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore,
+          quis! Laboriosam aliquam suscipit, error delectus, libero tempora
+          cupiditate reiciendis corporis laborum dignissimos porro consequatur
+          similique voluptatibus sint, commodi voluptate tenetur.
+        </p>
+
+        <button>BUY NOW</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
