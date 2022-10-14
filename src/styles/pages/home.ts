2c34fce9df2f0a53c535cb9d1ee9f731a3709f37
Agleylson Melo <agleylsonmelo@outlook.com>
2022-10-13 21:07:00 -0300

    Produto & Checkout - Estrutura de sucesso

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -8,7 +8,7 @@ export const HomeContainer = styled("main", {
   minHeight: 656,
 });
 
-export const Product = styled("a", {
+export const Product = styled("div", {
   background: "linear-gradient(180deg, #1ea463 0%, #7564d4 100%)",
   borderRadius: 8,
   cursor: "pointer",
